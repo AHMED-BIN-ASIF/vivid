0699cedf8e3edc31852ace2ada2eb29086f0f8e0
AHMED-BIN-ASIF <hafizahmed2007@gmial.com>
2024-11-23 18:58:29 +0500

    Lp Responsive completed

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,42 @@
+// Header TRop Slider  
+$('.number-items').slick({
+    dots: false,
+    arrows: false, // Corrected from `arrow` to `arrows`
+    infinite: true,
+    speed: 4000, // Speed of the sliding animation in milliseconds
+    autoplay: true,
+    autoplaySpeed: 0, // Continuous sliding without delay
+    cssEase: 'linear', // Smooth, linear sliding effect
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //     },
+    //   },
+    ],
+  });
+  
 // Header scroll js here 
 $(document).ready(function () {
     var s = $(".header-nav");
